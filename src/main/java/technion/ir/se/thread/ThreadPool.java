e1e7e2e81320ff79003d7ece8e745bed6bdf5ffd
Sapir Golan <s.golan@sap.com>
2014-05-27 09:55:54 +0300

    Added termination of Threads after all jobs were completed

--- src/main/java/technion/ir/se/thread/ThreadPool.java
+++ src/main/java/technion/ir/se/thread/ThreadPool.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPool {
 	private ExecutorService threadPool;
@@ -26,6 +27,8 @@ public class ThreadPool {
 			textFiles.add(submit);
 		}
 		
+		shutdownAndAwaitTermination();
+		
 		for (Future<List<File>> future : textFiles) {
 			try {
 				result.addAll(future.get());
@@ -39,5 +42,28 @@ public class ThreadPool {
 		}
 		return result;
 	}
+	
+	void shutdownAndAwaitTermination() {
+		threadPool.shutdown(); // Disable new tasks from being submitted
+		try {
+			int waitInSeconds = 60;
+			if (!terminateTasksAndCheckIfSucced(waitInSeconds)) {
+				threadPool.shutdownNow(); // Cancel currently executing tasks
+				if (!terminateTasksAndCheckIfSucced(waitInSeconds)) {
+					System.err.println("Pool did not terminate");
+				}
+			}
+		} catch (InterruptedException ie) {
+			// (Re-)Cancel if current thread also interrupted
+			threadPool.shutdownNow();
+			// Preserve interrupt status
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	private boolean terminateTasksAndCheckIfSucced(int secondsToWait) throws InterruptedException {
+		return threadPool.awaitTermination(secondsToWait, TimeUnit.SECONDS);
+	}
+
 
 }
