6f3c5560084989b79c2493458f6013ffcb52abe8
Sapir Golan <sapirgolan@gmail.com>
2014-06-03 17:28:04 +0300

    added getQueryTerms()

--- src/main/java/technion/ir/se/dao/Query.java
+++ src/main/java/technion/ir/se/dao/Query.java
@@ -1,12 +1,18 @@
 package technion.ir.se.dao;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Query {
 	private String id;
 	private String content;
+	private List<String> terms;
 	
 	public Query(String id, String content) {
 		this.id = id;
 		this.content = content;
+		this.terms = null;
 	}
 
 	public String getId() {
@@ -15,5 +21,13 @@ public class Query {
 
 	public String getQueryText() {
 		return content;
+	}
+	
+	public List<String> getQueryTerms() {
+		if (terms == null) {
+			String[] strings = content.split(" ");
+			terms = new ArrayList<String>(Arrays.asList(strings));
+		}
+		return terms;
 	}
 }
