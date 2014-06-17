0fa9b9a40aab6b92485d286f29e9e9f117b91478
Sapir Golan <s.golan@sap.com>
2014-06-17 10:51:39 +0300

    Baseline run also alternative queries

--- src/main/java/technion/ir/se/baseline/BaseLine.java
+++ src/main/java/technion/ir/se/baseline/BaseLine.java
@@ -3,9 +3,11 @@ package technion.ir.se.baseline;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Map;
 
 import technion.ir.se.Utils.Utils;
 import technion.ir.se.dao.Query;
+import technion.ir.se.dao.ResultFormat;
 import technion.ir.se.dao.RetrivalResult;
 import technion.ir.se.indri.SearchEngine;
 
@@ -13,15 +15,16 @@ public class BaseLine {
 	
 	public static final int NUMBER_OF_DOCUMNETS_TO_RETRIVE = 1000;
 	private SearchEngine engine;
+	private List<Query> queries;
 	
 	public BaseLine() {
 		engine = new SearchEngine();
+		queries = null;
 	}
 	
 	public void createBaseLine() {
 		//http://iew3.technion.ac.il/~kurland/sigir12-tutorial.pdf
 		String[] rules = new String[]{ "method:dir", "mu:1000", "fbDocs:50", "fbTerms:50", "fbOrigWeight:0.3", "fbMu:0"};
-		List<Query> queries;
 		try {
 			queries = Utils.readQueries();
 			StringBuilder trecMap = new StringBuilder();
@@ -45,6 +48,7 @@ public class BaseLine {
 		
 		
 	}
+	
 	private String createFileName(String[] rules) {
 		StringBuilder builder = new StringBuilder(30);
 		for (int i = 0; i < rules.length; i++) {
@@ -58,4 +62,46 @@ public class BaseLine {
 		
 		return fileName.substring(0, fileName.length() - 1);
 	}
+	
+	private String createFileName(String[] rules, String suffix) {
+		String fileName = this.createFileName(rules);
+		fileName += "_" + suffix;
+		return fileName;
+	}
+	
+	public void createAlternatives() {
+		try {
+			if (queries == null) {
+				queries = Utils.readQueries();
+			}
+			SemanticLogic logic = new SemanticLogic();
+			StringBuilder trecMap = new StringBuilder();
+			String[] rules = new String[]{ "method:dir", "mu:1000", "fbDocs:50", "fbTerms:50", "fbOrigWeight:0.3", "fbMu:0"};
+			for (Query query : queries) {
+				
+				List<RetrivalResult> results = engine.runQuery(NUMBER_OF_DOCUMNETS_TO_RETRIVE, rules, query.getQueryText());
+				Map<String, int[]> similarityVectors = logic.createSimilarityVectors(results, query);
+				List<Query> alternativeQuries = logic.createAlternativeQuries(similarityVectors, query);
+				List<ResultFormat> resultFormatsList = logic.submitAlternativeQuries(alternativeQuries);
+				
+				StringBuilder builder = Utils.createMapFormatForQuery(resultFormatsList);
+				trecMap.append(builder.toString());
+			}
+			
+			String fileName = createFileName(rules, "alternative");
+			Utils.writeMapFile(trecMap, fileName);
+			
+		} catch (IOException e) {
+			System.err.println("failed to read queris or write file");
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			System.err.println("failed to read queris");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.err.println("failed to run query");
+			e.printStackTrace();
+		}
+	}
+
+
 }
