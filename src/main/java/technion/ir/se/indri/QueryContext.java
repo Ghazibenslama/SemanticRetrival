8849707bf7cce86672c3a8c9e279dbb076ab2fa1
Sapir Golan <s.golan@sap.com>
2014-06-17 09:04:19 +0300

    removed unsued file

--- src/main/java/technion/ir/se/indri/QueryContext.java
+++
@@ -1,9 +0,0 @@
-package technion.ir.se.indri;
-
-public class QueryContext {
-	private int numberOfDocumentsToRetrived;
-	private String[] rules;
-	private String searchField;
-	private String query;
-	private String attributeNameToRetrive;
-}
