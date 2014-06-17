db500fee4abaa809203b9cb4db901af66a239e44
Sapir Golan <sapirgolan@gmail.com>
2014-06-17 19:38:57 +0300

    Added parameter for folder path that contains TREC files

--- src/main/java/main/ParseTrecDocuments.java
+++ src/main/java/main/ParseTrecDocuments.java
@@ -13,10 +13,14 @@ public class ParseTrecDocuments {
 	 */
 	public static void main(String[] args) {
 
+		if (args.length == 0) {
+			System.err.println("You didn't given a path to dir with files");
+			System.exit(1);
+		}
 		long startTime = System.currentTimeMillis();
 		System.out.println(String.format("Job started at: %s", getCurrentTime(startTime)));
 		TrecDocumentCreator documentCreator = new TrecDocumentCreator();
-		documentCreator.convertTrecDocumentsToTextDocuments("C:\\Temp\\InformationRetrival\\docs\\FBIS");
+		documentCreator.convertTrecDocumentsToTextDocuments(args[0]);
 		long endTime = System.currentTimeMillis();
 		System.out.println(String.format("Job ended at: %s", getCurrentTime(endTime)));
 		System.out.println(String.format("Total tome for executing Job: %s", getCurrentTime(endTime-startTime)));
