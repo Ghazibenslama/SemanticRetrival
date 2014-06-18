3882490524351ba3d2c3b5d4a7fe9f85dabc258a
Sapir Golan <sapirgolan@gmail.com>
2014-06-18 20:20:27 +0300

    Changed POM to generate JAR files that runs Experiments

---
+++ src/main/java/main/RunExperiments.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.io.IOException;
+
+import technion.ir.se.baseline.BaseLine;
+
+public class RunExperiments {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		if (args.length > 0) {
+			System.out.println("you are in performance\\debugging mode, press any key to continue");
+			System.in.read();
+		}
+		BaseLine baseLine = new BaseLine();
+//		baseLine.createBaseLine();
+		baseLine.createAlternatives();
+	}
+
+}
