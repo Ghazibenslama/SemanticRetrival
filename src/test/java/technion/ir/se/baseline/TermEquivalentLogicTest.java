5bef82aa551109b9e1107dc244a1aa68e97af58a
Sapir Golan <sapirgolan@gmail.com>
2014-06-20 10:51:29 +0300

    Fixed tests and logic of TermEquivalentLogic.java

--- src/test/java/technion/ir/se/baseline/TermEquivalentLogicTest.java
+++ src/test/java/technion/ir/se/baseline/TermEquivalentLogicTest.java
@@ -1,35 +1,53 @@
 package technion.ir.se.baseline;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
+import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
 import technion.ir.se.Model.Model;
-import technion.ir.se.Utils.Utils;
 import technion.ir.se.dao.SemanticTermScore;
 import technion.ir.se.exception.VectorLengthException;
 
+import com.google.common.collect.ImmutableMap;
+
+@PrepareForTest(Model.class)
+@RunWith(PowerMockRunner.class)
 public class TermEquivalentLogicTest {
 	private TermEquivalentLogic classUnderTest;
-	private double[] qTermVector;
-	private int[] firstTermVector;
-	private int[] secondTermVector;
-	private int[] thirdTermVector;
-	Map<String, Map<String, Short>> documentTerms;
+	private static final String ALEF = "alef";
+	private static final String BETH = "beth";
+	private static final String GIMEL = "gimel";
+	private static final String DALED = "Daled";
+	private static final String HEI = "Hei";
+	private static final String QUERY_TERM = "q";
+	private Map<String, Map<String, Short>> outerMap;
 	
 	@Before
 	public void setUp() throws Exception 
 	{
 		classUnderTest = new TermEquivalentLogic();
+		outerMap = new HashMap<String, Map<String,Short>>();
+		
+		List<String> list = Arrays.asList(ALEF, BETH, GIMEL, DALED, HEI);
+		Model mockedModel = PowerMockito.mock(Model.class);
+		PowerMockito.when(mockedModel.getModel()).thenReturn(list);
+		
+		PowerMock.mockStatic(Model.class);
+		EasyMock.expect(Model.getInstance()).andReturn(mockedModel).anyTimes();
+		PowerMock.replay(Model.class);
 	}
 
 	@After
@@ -39,20 +57,15 @@ public class TermEquivalentLogicTest {
 	@Test
 	public void testTermEquivalentLogic() throws VectorLengthException 
 	{
-		qTermVector = new double[] {2.0,1.0,0.0};
-		firstTermVector = new int[] {3,0,0};
-		secondTermVector = new int[] {2,1,1};
-		thirdTermVector = new int[] {1,1,1};
-		documentTerms = new TreeMap<String, Map<String, Short>>();
-		documentTerms.put("t1", Utils.convertIntArrtoDoubleArr(firstTermVector));
-		documentTerms.put("t2", Utils.convertIntArrtoDoubleArr(secondTermVector));
-		documentTerms.put("t3", Utils.convertIntArrtoDoubleArr(thirdTermVector));
+		outerMap.put(QUERY_TERM, ImmutableMap.of(ALEF, (short)2, BETH, (short)1));
+		outerMap.put(ALEF, ImmutableMap.of(ALEF, (short) 3, BETH, (short)0, GIMEL, (short)0));
+		outerMap.put(BETH, ImmutableMap.of(ALEF, (short) 2, BETH, (short)1, GIMEL, (short)1));
+		outerMap.put(GIMEL, ImmutableMap.of(ALEF, (short) 1, BETH, (short)1, GIMEL, (short)1));
 		
-		List<SemanticTermScore> termScore = new ArrayList<SemanticTermScore>();
-		termScore = classUnderTest.similarVectors(documentTerms, qTermVector);
+		List<SemanticTermScore> termScore = classUnderTest.similarVectors(outerMap, QUERY_TERM);
 		Assert.assertEquals("result contains 3 items", 3, termScore.size());
-		Assert.assertEquals("First argument is not correct", "t2", termScore.get(0).getTerm());
-		Assert.assertEquals("last argument is not correct", "t3", termScore.get(2).getTerm());
+		Assert.assertEquals("First argument is not correct", BETH, termScore.get(0).getTerm());
+		Assert.assertEquals("last argument is not correct", GIMEL, termScore.get(2).getTerm());
 		Assert.assertFalse("First Score not in range", termScore.get(1).getSemanticScore() < 0.89 ||
 														termScore.get(1).getSemanticScore() > 0.895 );
 		
@@ -61,17 +74,13 @@ public class TermEquivalentLogicTest {
 		
 		Assert.assertFalse("Thirs Score not in range", termScore.get(2).getSemanticScore() < 0.77 ||
 														termScore.get(2).getSemanticScore() > 0.775 );
-
 	}
 	
 	@Test
 	public void testConvertMapToVector () throws Exception {
-		List<String> list = Arrays.asList("alef", "beth", "gimel", "Daled", "Hei");
-		Model.getInstance().setModel(list);
-		
 		Map<String, Short> map = new HashMap<String, Short>();
-		map.put("alef", (short) 5);
-		map.put("gimel", (short) 2);
+		map.put(ALEF, (short) 5);
+		map.put(GIMEL, (short) 2);
 		
 		double[] vectorOfTerm = Whitebox.<double[]>invokeMethod(classUnderTest, "convertMapToVector", map);
 		double[] expected = new double[]{5,0,2,0,0};
