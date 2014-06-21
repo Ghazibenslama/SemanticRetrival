d1f0766b557ddf123d562882e34fb2c13d3f2024
Sapir Golan <sapirgolan@gmail.com>
2014-06-21 11:02:35 +0300

    Added tests for 'compareTo(SemanticTermScore otherResultFormat)' method

---
+++ src/test/java/technion/ir/se/dao/SemanticTermScoreTest.java
@@ -0,0 +1,90 @@
+package technion.ir.se.dao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SemanticTermScoreTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCompareTo_positiveValues() {
+		SemanticTermScore a = new SemanticTermScore("a", 0.9);
+		SemanticTermScore b = new SemanticTermScore("b", 0.8);
+		SemanticTermScore c = new SemanticTermScore("c", 0.7);
+		Assert.assertEquals("a is bigger than b", -1l, a.compareTo(b));
+		Assert.assertEquals("b is bigger than c", -1l, b.compareTo(c));
+		Assert.assertEquals("a is bigger than c", -1l, a.compareTo(c));
+		
+		Assert.assertEquals("b is smaller than a", 1l, b.compareTo(a));
+		Assert.assertEquals("c is smaller than a", 1l, c.compareTo(a));
+		Assert.assertEquals("c is smaller than b", 1l, c.compareTo(b));
+	}
+	
+	@Test
+	public void testCompareTo_negativeValues() {
+		SemanticTermScore a = new SemanticTermScore("a", -0.1);
+		SemanticTermScore b = new SemanticTermScore("b", -0.6);
+		SemanticTermScore c = new SemanticTermScore("c", -0.7);
+		Assert.assertEquals("a is bigger than b", -1l, a.compareTo(b));
+		Assert.assertEquals("b is bigger than c", -1l, b.compareTo(c));
+		Assert.assertEquals("a is bigger than c", -1l, a.compareTo(c));
+		
+		Assert.assertEquals("b is smaller than a", 1l, b.compareTo(a));
+		Assert.assertEquals("c is smaller than a", 1l, c.compareTo(a));
+		Assert.assertEquals("c is smaller than b", 1l, c.compareTo(b));
+	}
+	
+	@Test
+	public void testCompareTo_nullValues() {
+		SemanticTermScore a = new SemanticTermScore("a", 3.5);
+		SemanticTermScore b = null;
+		SemanticTermScore c = new SemanticTermScore("c", 0.7);
+		Assert.assertEquals("a is bigger than b", -1l, a.compareTo(b));
+		Assert.assertEquals("a is bigger than c", -1l, a.compareTo(c));
+		
+		Assert.assertEquals("c is smaller than a", 1l, c.compareTo(a));
+		Assert.assertEquals("c is smaller than b", -1l, c.compareTo(b));
+	}
+	
+	@Test
+	public void testCompareTo_equalValues() {
+		SemanticTermScore a = new SemanticTermScore("a", 3.5);
+		SemanticTermScore c = new SemanticTermScore("c", 3.5);
+		
+		Assert.assertEquals("a is equal to c", 0l, a.compareTo(c));
+		Assert.assertEquals("c is equal to a", 0l, c.compareTo(a));
+	}
+
+	@Test
+	public void testCompareTo_CollectionSort() {
+		SemanticTermScore a = new SemanticTermScore("a", 0.9);
+		SemanticTermScore b = new SemanticTermScore("b", 0.8);
+		List<SemanticTermScore> list = new ArrayList<SemanticTermScore>();
+		list.add(a);
+		list.add(b);
+		Collections.sort(list);
+		Assert.assertEquals("first object in list after sort should be a", a, list.get(0));
+		Assert.assertEquals("second object in list after sort should be b", b, list.get(1));
+		
+		list.clear();
+		list.add(b);
+		list.add(a);
+		Collections.sort(list);
+		Assert.assertEquals("first object in list after sort should be a", a, list.get(0));
+		Assert.assertEquals("second object in list after sort should be b", b, list.get(1));
+		
+	}
+}
