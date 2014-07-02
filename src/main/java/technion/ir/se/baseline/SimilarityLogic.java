cb2ea36dcb6320d3f2c3ab3389d1457dd3905838
Sapir Golan <sapirgolan@gmail.com>
2014-07-02 22:15:07 +0300

    Added 'public double calculateSimilarity(Map<String, Short> vectorOne, Map<String, Short> vectorTwo)' method and its private methods

--- src/main/java/technion/ir/se/baseline/SimilarityLogic.java
+++ src/main/java/technion/ir/se/baseline/SimilarityLogic.java
@@ -1,9 +1,53 @@
 package technion.ir.se.baseline;
 
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
 import technion.ir.se.exception.VectorLengthException;
 
-public class SimilarityLogic
-{
+public class SimilarityLogic {
+	
+	/**
+	 * Calculates <b>cosine</b> similarity of two vectors that are saved as Map
+	 * <br>
+	 * COSINE similarity - normalized dot product
+	 * 
+	 * @param vectorOne Map representation  of vector one
+	 * @param vectorTwo Map representation  of vector two
+	 * @return Similarity of two vectors
+	 */
+	public double calculateSimilarity(Map<String, Short> vectorOne, Map<String, Short> vectorTwo) {
+		Set<String> both = Sets.newHashSet(vectorOne.keySet());
+        both.retainAll(vectorTwo.keySet());
+        
+        double sclar = 0, norm1 = 0, norm2 = 0;
+        
+        for (String k : both) {
+        	sclar += vectorOne.get(k) * vectorTwo.get(k);
+        }
+        norm1 = calculateNorm(vectorOne);
+        norm2 = calculateNorm(vectorTwo);
+        
+        return sclar / (norm1 * norm2);
+	}
+	
+	/**
+	 * Calculates the Norm of a given vector which is represented by {@link Map} of <{@link String}, {@link Short}>
+	 * @param {@link Map} of <{@link String}, {@link Short}> which is actually a vector
+	 * @return norm of a vector
+	 * 
+	 */
+
+	private double calculateNorm(Map<String, Short> vectorOne) {
+		double norm = 0;
+		for (String k : vectorOne.keySet()) {
+			norm += vectorOne.get(k) * vectorOne.get(k);
+        }
+		return Math.sqrt(norm);
+	}
+	
 	/**
 	 * Calculates similarity of type given between two vectors
 	 * <br>
--- src/test/java/technion/ir/se/baseline/SimilarityLogicTest.java
+++ src/test/java/technion/ir/se/baseline/SimilarityLogicTest.java
@@ -1,9 +1,14 @@
 package technion.ir.se.baseline;
 
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import com.google.common.collect.ImmutableMap;
 
 import technion.ir.se.exception.VectorLengthException;
 
@@ -20,7 +25,7 @@ public class SimilarityLogicTest {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
+/*	@Test
 	public void testCalculateSimilarity_equalVectors() throws VectorLengthException {
 		double[] vec1 = new double[]{1,2,3,4,5,6};
 		double[] vec2 = new double[]{1,2,3,4,5,6};
@@ -43,6 +48,51 @@ public class SimilarityLogicTest {
 		double expected = 311.74 / (9.66229786334 * 33.8777212929);
 		double similarity = classUnderTest.calculateSimilarity(vec1, vec2);
 		Assert.assertEquals("cosine value is not as expected", expected, similarity, 0.0001);
+	}*/
+	
+	@Test
+	public void testCalculateSimilarity_equalVectors() throws VectorLengthException {
+		Map<String, Short> vector = ImmutableMap.of("a", (short)1, "b", (short)2, "c", (short)3, "d", (short)4, "e", (short)5);
+//		double[] vec1 = new double[]{1,2,3,4,5,6};
+//		double[] vec2 = new double[]{1,2,3,4,5,6};
+		double similarity = classUnderTest.calculateSimilarity(vector, vector);
+		Assert.assertEquals("cosine of equal vectors should be 1", 1, similarity, 0);
+	}
+	
+	@Test
+	public void testCalculateSimilarity_differentVectors() throws VectorLengthException {
+		Map<String, Short> vectorOne = ImmutableMap.of("a", (short)1, "b", (short)2, "c", (short)3, "d", (short)0, "e", (short)0);
+		Map<String, Short> vectorTwo = ImmutableMap.of("a", (short)0, "b", (short)0, "c", (short)0, "d", (short)4, "e", (short)5);
+
+		double similarity = classUnderTest.calculateSimilarity(vectorOne, vectorTwo);
+		Assert.assertEquals("cosine of different vectors should be 0", 0, similarity, 0);
+	}
+	
+	@Test
+	public void testCalculateSimilarity_someVectorsWithZeroes() throws VectorLengthException {
+		Map<String, Short> vectorOne = ImmutableMap.of("a", (short)1, "b", (short)2, "c", (short)0, "d", (short)9, "e", (short)0);
+		Map<String, Short> vectorTwo = ImmutableMap.of("a", (short)0, "b", (short)3, "c", (short)4, "d", (short)33, "e", (short)6);
+		
+		double expected = 303 / (9.2736184955 * 33.9116499156);
+		double similarity = classUnderTest.calculateSimilarity(vectorOne, vectorTwo);
+		Assert.assertEquals("cosine value is not as expected", expected, similarity, 0.0001);
+	}
+	
+	@Test
+	public void testCalculateSimilarity_someVectorsWithOutZeroes() throws VectorLengthException {
+		Map<String, Short> vectorOne = ImmutableMap.of("a", (short)1, "b", (short)2, "d", (short)9);
+		Map<String, Short> vectorTwo = ImmutableMap.of("b", (short)3, "c", (short)4, "d", (short)33, "e", (short)6);
+		
+		double expected = 303 / (9.2736184955 * 33.9116499156);
+		double similarity = classUnderTest.calculateSimilarity(vectorOne, vectorTwo);
+		Assert.assertEquals("cosine value is not as expected", expected, similarity, 0.0001);
+	}
+	
+	@Test
+	public void tetsCalculateNorm() throws Exception {
+		Map<String, Short> vector = ImmutableMap.of("a", (short)2, "b", (short)1, "c", (short)0, "d", (short)7);
+		Double norm = Whitebox.<Double>invokeMethod(classUnderTest, "calculateNorm", vector);
+		Assert.assertEquals("norm of vector is wrong", 7.34847, norm, 0.0001);
 	}
 
 
