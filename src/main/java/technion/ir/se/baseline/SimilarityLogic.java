aa10244aacfc31cda1f0771a87c32e88dc5b7433
Sapir Golan <sapirgolan@gmail.com>
2014-07-02 22:23:22 +0300

    Removed method 'public double calculateSimilarity(double[] vec1, double[] vec2)'  that uses vectors to compare

--- src/main/java/technion/ir/se/baseline/SimilarityLogic.java
+++ src/main/java/technion/ir/se/baseline/SimilarityLogic.java
@@ -5,8 +5,6 @@ import java.util.Set;
 
 import com.google.common.collect.Sets;
 
-import technion.ir.se.exception.VectorLengthException;
-
 public class SimilarityLogic {
 	
 	/**
@@ -47,47 +45,4 @@ public class SimilarityLogic {
         }
 		return Math.sqrt(norm);
 	}
-	
-	/**
-	 * Calculates similarity of type given between two vectors
-	 * <br>
-	 * COSINE similarity - normalized dot product
-	 * 
-	 * @param vec1 Array of doubles for first vector
-	 * @param vec2 Array of doubles for second vector
-	 * @return the distance between the two vectors
-	 * @throws VectorLengthException 
-	 */
-	public double calculateSimilarity(double[] vec1, double[] vec2) throws VectorLengthException {
-		double similarity = 0;
-		if (vec1.length != vec2.length)
-		{
-			throw new VectorLengthException("Vectors Lengths are not equals");
-		}
-		for (int i = 0; i < vec1.length; i++) 
-			{
-				similarity += vec1[i] * vec2[i];
-			}
-	    
-		similarity = similarity / (vectorLength(vec1) * vectorLength(vec2));
-
-		
-		return similarity;
-	}
-	
-	/**
-	 * Calculates the length of the given vector
-	 * @param vec double array of the vector
-	 * @return length of the vector as a double
-	 * 
-	 */
-	private static double vectorLength(double[] vec) {
-		double len = 0;
-		for (int i = 0; i < vec.length; i++) {
-			len += vec[i] * vec[i];
-		}
-		len = Math.sqrt(len);
-		return len;			
-	}
-	
 }
