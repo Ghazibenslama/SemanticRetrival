16ec88b2bd3b779777ac3f74b652f836ec3bd19b
Sapir Golan <sapirgolan@gmail.com>
2014-08-23 11:53:41 +0300

    Implimented 'Template Method pattern'; 'List<List<String>> findPhrases(Query query)' can be overide by any subclass of PairsRelatedLogic; PairsRelatedLogicThreshold overides 'List<List<String>> findPhrases(Query query)' method

--- src/main/java/technion/ir/se/baseline/PairsRelatedLogic.java
+++ src/main/java/technion/ir/se/baseline/PairsRelatedLogic.java
@@ -5,43 +5,54 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import technion.ir.se.dao.Query;
 
 public class PairsRelatedLogic
  {
-	private MutualInformationLogic mil;
+	
+	private final Logger logger = Logger.getLogger(PairsRelatedLogic.class);
 	private List<Query> queries;
 	
-	public PairsRelatedLogic (MutualInformationLogic mil , List<Query> queries )
-	{
+	protected MutualInformationLogic mil;
+	
+	public PairsRelatedLogic (MutualInformationLogic mil , List<Query> queries ) {
 		this.mil = mil;
 		this.queries = queries;
 	}
 	
-	public Map<String, List<List<String>>> clacRelatedPairs()
-	{
+	/**
+	 * Override this method if you wish to change the way phrases are constructed for each query
+	 * @param query
+	 * @return
+	 * @throws Exception
+	 */
+	protected List<List<String>> findPhrases(Query query) throws Exception {
+		return mil.findPhrases(query);
+	}
+	
+	/**
+	 * The method defines a template of 'findRelatedPairs' of terms that have some 'connection' between them.<br>
+	 * You can change the way term are constructed as 'pairs' by overriding {@link #findPhrases(Query)}
+	 * @return
+	 */
+	public final Map<String, List<List<String>>> findRelatedPairs() {
 		Map<String, List<List<String>>> pairRelatedMap = new HashMap<String,List<List<String>>>(); 
-		//List<PairsRelated> pairRelatedList = new ArrayList <PairsRelated>();
-		for (Query query : queries)
-		{
-			ArrayList<List<String>> resultLists = new ArrayList<List<String>>();
-			try 
-			{
-				resultLists = (ArrayList<List<String>>) mil.findPhrases(query);
-			} 
-			catch (Exception e) 
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		for (Query query : queries) {
+			List<List<String>> resultLists = new ArrayList<List<String>>();
+			try  {
+				resultLists = this.findPhrases(query);
+			} catch (Exception e) {
+				logger.error("Failed to find pairs of terms (phrases) that have high Mutual Information value" , e);
 			}
-			if (resultLists.size() > 0)
-				for (List<String> list : resultLists) 
-				{
-					pairRelatedMap.put(mil.getQueryID(), resultLists);
-				}
+			
+			if (resultLists.size() > 0) {
+				pairRelatedMap.put(mil.getQueryID(), resultLists);
+			}
 				
 		}
 		return pairRelatedMap;
 	}
-
+	
  }
---
+++ src/main/java/technion/ir/se/baseline/PairsRelatedLogicThreshold.java
@@ -0,0 +1,27 @@
+package technion.ir.se.baseline;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import technion.ir.se.dao.Query;
+
+public class PairsRelatedLogicThreshold extends PairsRelatedLogic {
+
+	private double threshold;
+	private final Logger logger = Logger.getLogger(PairsRelatedLogicThreshold.class);
+
+
+	public PairsRelatedLogicThreshold(MutualInformationLogic mil, List<Query> queries, double threshold) {
+		super(mil, queries);
+		this.threshold = threshold;
+	}
+
+	@Override
+	protected List<List<String>> findPhrases(Query query) throws Exception {
+		logger.info("finding phrases with threshold: " + threshold);
+		return mil.findPhrases(query, threshold);
+	}
+
+	
+}
