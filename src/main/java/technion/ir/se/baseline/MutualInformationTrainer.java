bca987fec1bce41c9b0666788094b82cd49ea92c
Sapir Golan <sapirgolan@gmail.com>
2014-08-30 17:40:13 +0300

    Changed threshold of training

--- src/main/java/technion/ir/se/baseline/MutualInformationTrainer.java
+++ src/main/java/technion/ir/se/baseline/MutualInformationTrainer.java
@@ -34,7 +34,7 @@ public class MutualInformationTrainer extends BaseLine implements IParamTrainer 
 			String qrelsFileName = Utils.readProperty("qrels.file");
 			logger.debug("Finshed reading qrels file: " + qrelsFileName);
 			TrecEvalDataFile goldResults = parser.convertFile(qrelsFileName);
-			for (double threshold = 0.1; threshold <= 0.6; threshold+=0.1) {
+			for (double threshold = 0.05; threshold <= 0.55; threshold+=0.05) {
 				this.threshold = threshold;
 				logger.info("About to run Training of MutualInformationTrainer with threshold=" + this.threshold);
 				File mutualInformationResults = this.createAlternatives(5);
